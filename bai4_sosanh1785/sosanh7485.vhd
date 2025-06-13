0abe822347ff0f50c17c6cca3a0199eee195944e
Tuong <tuongkhak@gmail.com>
2025-06-13 16:29:50 +0700

    main

--- bai4_sosanh1785/sosanh7485.vhd
+++ bai4_sosanh1785/sosanh7485.vhd
@@ -1,53 +1,31 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_arith.all;
-use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
 
 entity sosanh7485 is
-	port(
-		A:in std_logic_vector(3 downto 0);
-		B:in std_logic_vector(3 downto 0);
-		I:in std_logic_vector(2 downto 0);
-		O:out std_logic_vector(2 downto 0)
-		);
+    port (
+        A: in std_logic_vector(3 downto 0);
+        B: in std_logic_vector(3 downto 0);
+        I: in std_logic_vector(2 downto 0);
+        O: out std_logic_vector(2 downto 0)
+    );
 end entity;
 
 architecture Behavioral of sosanh7485 is
-begin 
-	process(A,B,I)
-		begin 
-			if (A(3)>B(3)) then
-				O<="100";
-			elsif A(3)<B(3) then
-				O<="010";
-			elsif A(3)=B(3) then	
-				if A(2)>B(2) then
-				O<="100";
-				elsif A(2)<B(2) then
-				O<="010";
-				elsif A(2)=B(2) then
-						if A(1)>B(1) then
-							O<="100";
-						elsif A(1)<B(1) then
-							O<="010";
-						elsif A(1)=B(1) then
-							if A(0)>B(0) then
-								O<="100";
-							elsif A(0)<B(0) then
-								O<="010";
-							elsif A(0)=B(0) then
-								case I is 
-									when "100"=>O<="100";
-									when "010"=>O<="010";
-									when "001"=>O<="001";
-									when others=>O<="XXX";
-								end case;
-							end if;
-						end if;
-				end if;
-			end if;
-		end process;
-	 end Behavioral;
-					
-				
-									
+begin
+    process(A, B, I)
+    begin
+        if unsigned(A) > unsigned(B) then-- chuyển vector về dạng số nguyên không dấu
+            O <= "100";
+        elsif unsigned(A) < unsigned(B) then
+            O <= "010";
+        else
+            case I is
+                when "100" => O <= "100";
+                when "010" => O <= "010";
+                when "001" => O <= "001";
+                when others => O <= "XXX";
+            end case;
+        end if;
+    end process;
+end Behavioral;
